91411f8fe0f0c0615ec4ca7180613bc3ef664d4c
Amy Xu <xuamyj@gmail.com>
2024-06-29 22:32:36 -0400

    first supabase db query

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -17,9 +17,21 @@ export default async function AuthButton() {
     return redirect("/login");
   };
 
-  return user ? (
+  let displayName = null;
+  if (user) {
+    const { data, error } = await supabase
+    .from('user_info')
+    .select()
+    .eq('id', user.id);
+
+    if (data && data.length > 0) {
+      displayName = data[0].display_name;
+    }
+  }
+
+  return displayName ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {displayName}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
