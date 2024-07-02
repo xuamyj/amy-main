c4180b60aec8070ddb716d36b8a2473190e4be73
Amy Xu <xuamyj@gmail.com>
2024-07-01 22:40:26 -0400

    D pstern time CODE HELP

--- utils/supabase/amy/helpers.ts
+++ utils/supabase/amy/helpers.ts
@@ -7,7 +7,10 @@ import { Tables, TablesInsert, TablesUpdate } from "@/types/supabase";
 // --------------
 
 export function getTodayYYYYMMDD() {
-  const currentDate = new Date();
+  const options = { timeZone: 'America/Los_Angeles' };
+  const rawDate = new Date().toLocaleString('en-US', options);
+  const currentDate = new Date(rawDate);
+
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-based
   const day = String(currentDate.getDate()).padStart(2, '0');
